6fb2b33302670b9ff1b325a5310af3b9991936d2
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-06-19 12:27:07 +0530

    variables01

---
+++ 02-variables01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Name1:teja
+Name2:siva
+echo "$Name1::Hello $Name2, How are you?"
+echo "$Name2::Hi $Name1, I am fine how are you?"
+echo "$Name1::I am fine too. How is your work?"
+echo "$Name2::Not bad. I am thinking to upgrade to Devops"
