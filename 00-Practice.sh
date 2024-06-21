0bf95ee04233c36f6c2e7b2638d24275324e9d6e
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-06-21 23:19:34 +0530

    practice purpose

---
+++ 00-Practice.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+person1=$1
+person2=$2
+$person1:hi $person2
+$person2:Hi $person1 I am fine how are you $person1
+$person1:$person2 how happed your intrivew
+$person2:yaa $person1 it was happed very smooth.
