06bb0e3717633a54f7772cb72de68d841d77c7fa
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-06-21 23:24:41 +0530

    practice

--- 00-Practice.sh
+++ 00-Practice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash/
 person1=$1
 person2=$2
-$person1:hi $person2
-$person2:Hi $person1 I am fine how are you $person1
-$person1:$person2 how happed your intrivew
-$person2:yaa $person1 it was happed very smooth.
+echo "$person1:hi $person2"
+echo "$person2:Hi $person1 I am fine how are you $person1"
+echo "$person1:$person2 how happed your intrivew"
+echo "$person2:yaa $person1 it was happed very smooth."
