b2532c748ac8448d6a227f0075ccaa17f5afb3b9
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-06-21 23:33:34 +0530

    variables 2nd example

---
+++ 02-variables02.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+person1=$1
+person2=$2
+echo "$person1:hi $person2"
+echo "$person2:Hi $person1 how are you $person1"
+echo "$person1:$person2 I am fine how happed your intrivew"
+echo "$person2:yaa $person1 it was happed very smooth."
+echo "$person1:when you will get offer letter $person2
+echo "$person2:Next week i have HR round $person1 after completing that will release"
