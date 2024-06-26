4f841417c6c45b9c031fb25b8e72b3556b4c49f7
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-06-26 11:23:54 +0530

    practicing

--- 00-practice01.sh
+++ 00-practice01.sh
@@ -1,7 +1,5 @@
-Userid=$1
-password=$2
 echo "please enter your userid:"
-read -s $Userid
+read -s Userid
 echo "please enter your password:"
-read -s $password
+read -s password
 echo "Your user id is:$Userid & Password is:$password"
