e5681c266e507f6d8700587b009c5d896b69f15b
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-06-26 10:41:43 +0530

    array

---
+++ 03-Arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+Movies=("RRR" "DJTILLU" "MURARI")
+echo "First movie is:${Movies[0]}"
+echo "Second movie is:${Movies[1]}"
+echo "third movie is:${Movies[2]}"
+echo "All movies are:${Movies[@]}" 
