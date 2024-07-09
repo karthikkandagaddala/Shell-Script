5ca4e43f51ceca02bd2385a3b3deee2346d64798
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-07-09 10:19:16 +0530

    Special variables

---
+++ 04-Special variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "All variables:$@"
+echo "Script name:$0"
+echo "Currect working directory:$pwd"
+echo "Hostname:$hostname"
+echo "Home directory of current user:$home"
+echo "Which user is running this script:$user"
+echo "process ID of the current shell script:$$"
+echo "process ID of last background command:$!"
+echo "Previous status exit status:$?"
