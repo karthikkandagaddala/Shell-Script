d7348290238072c2845fa984147afc03015c8642
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-07-09 11:03:51 +0530

    Special variables

---
+++ 05-Condition.sh
@@ -0,0 +1,13 @@
+Number=$1
+if [$Number -gt 10]
+then
+echo "Given number $Number is greather than 10"
+else
+echo "Given number $Number is less than 10"
+fi
+
+#-gt=greather than
+#-lt=lesser than
+#-eq=equal
+#-greather than equal
+#-lesser than equal
