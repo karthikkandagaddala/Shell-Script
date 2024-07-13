5db5925749cd656d29fa993475b3b8920c3d8598
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-07-13 12:13:03 +0530

    practice

--- 00-practice00.sh
+++ 00-practice00.sh
@@ -1,6 +1,6 @@
 Number=$1
 if [ $Number -gt 10 ]; then
-  echo($Number is greather than 10)
+  echo "$Number is greather than 10"
 else
-  echo($Number is lesser than 10)
+  echo "$Number is lesser than 10"
 fi
