639885eb8aca67e2e49225aa3d8e6b10bb39753f
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-07-13 23:44:04 +0530

    practice

--- 00-Practice.sh
+++ 00-Practice.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 userid=$(id -u)
+timestamp=$(date +%F-%H-%M-%s)
 if [ $userid -ne 0 ]
 then
-    echo "Please run with sudo access"
+    echo "Please proced with sudo access"
     exit 1
 else
-    echo "Your super user"
+    echo "your a super user"
 fi
+valid(){
+    fi [ $1 -ne 0 ]
+    then
+        echo "$2....FAILURE"
+        exit 1
+    else
+        echo "$2....SUCCESS"
+    fi    
+}
 dnf install mysql -y
+valid $? "Installing Mysql"
+dnf install git -y
+valid $? "Installing git"
