2c40fd1774f06b6868d787016d75a955b42f0206
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-07-15 23:29:36 +0530

    practice function

--- 00-practice00.sh
+++ 00-practice00.sh
@@ -9,15 +9,15 @@ else
     echo "Your a root user"
 fi
 function(){
-    if [ $? -ne 0 ]
-    then
-        echo "$2....Failure"
-        exit 1
-    else
-        echo "$2....Completed"
-    fi
+  if [ $1 -ne 0 ]
+  then
+      echo "$2....Failure"
+      exit 1
+  else
+      echo "$2....Completed"
+  fi
 }
 dnf install mysql -y
-function $1 "Installing MYSQL"
+function $? "Installing MYSQL"
 dnf install Docker -y
-function $1 "Instaling Docker"
+function $? "Instaling Docker"
