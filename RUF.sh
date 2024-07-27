a05bd6b85520d724d99e06d122dbaf1dda041f21
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-07-27 16:09:28 +0530

    ruf

--- RUF.sh
+++ RUF.sh
@@ -1,1 +1,10 @@
-dnf remove my sql -y
+#!/bin/bash
+userid=$(id -u)
+timestamp=$(date +%F-%H-%M-%S)
+scriptname=$(echo $0 | cut -d "." -f1)
+logfile=/tmp/$scriptname-$timestamp.log
+r="\e[31m"
+g="\e[32m"
+n="\e[0m"
+echo -e "$r print in red colour $n"
+echo -e "$G print in green colour$n" 
