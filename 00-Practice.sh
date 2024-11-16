5565e09a859b0b903c615a0f872a921f2709dc17
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-11-16 15:59:58 +0530

    practice

--- 00-Practice.sh
+++ 00-Practice.sh
@@ -1,5 +1,10 @@
-#! /bash/bin/
-person1=karthik     
-person2=teja
-echo "$person1::Hi $person2 how are?"
-echo "$person2::$person1 I am good, how about you?"
+#! /bin/bash/
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+      echo "Please start with Sudo access"
+      exit 1
+else
+     echo "You are a super user"
+fi
+dnf install mysql -y
