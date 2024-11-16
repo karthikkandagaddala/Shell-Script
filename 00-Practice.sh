11727db3284e1131e85f939b3f18e850445683e1
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-11-16 15:05:47 +0530

    practice

--- 00-Practice.sh
+++ 00-Practice.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 person1=$1
 Person2=$2
-echo "$person1::Hello $person2, how are you?"
-echo "$person2::Hi $person1, i am fine how are?"
+echo "$ person1::Hello $ person2, how are you?"
+echo "$ person2::Hi $ person1, i am fine how are?"
