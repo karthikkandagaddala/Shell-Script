7d4b3f9a40824f94cbb897a993d748e7ed81b8eb
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-11-16 14:58:05 +0530

    practice

--- 00-Practice.sh
+++ 00-Practice.sh
@@ -1,25 +1,5 @@
-#!/bin/bash
-userid=$(id -u)
-timestamp=$(date +%F-%H-%M-%S)
-script_name=$(echo $0 | cut -d "." -f1)
-logfile=/tmp/$script_name-$timestamp.log
-if [ $userid -ne 0 ]
-then
-     echo "Please run this script as a sudo access"
-     exit 1
-else
-    echo "your a super user"
-fi
-boom(){
-    if [ $1 -ne 0 ]
-    then
-         echo "$2....is Failure."
-         exit 1
-    else
-         echo "$2....is Success"
-    fi
-}
-dnf install mysql -y &>>$logfile
-boom $? "Installing my sql"
-dnf install docker -y &>>$logfile
-boom $? "installing docker"
+#! /bin/bash
+person1=$1
+Person2=$2
+echo "$person1::Hello $person2, how are you?"
+echo "$person2::Hi $person1, i am fine how are?"
