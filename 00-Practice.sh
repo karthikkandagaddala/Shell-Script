b6ffe6a511823c2b28eac646d4bafcc9924c25a5
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-11-16 16:25:12 +0530

    practice

--- 00-Practice.sh
+++ 00-Practice.sh
@@ -2,9 +2,24 @@
 userid=$(id -u)
 if [ $userid -ne 0 ]
 then
-      echo "Please start with Sudo access"
+      echo "Please start with SUDO USER"
       exit 1
 else
-     echo "You are a super user"
+      echo "Your a ***SUDO USER***"
 fi
 dnf install mysql -y
+if [ $? -ne 0 ]
+then
+      echo "My sql install is failure"
+      exit 1
+else
+      echo "MY Sql installation is success"
+dnf install ansible -y
+if [ $? -ne 0 ]
+then 
+      echo "Ansible install failure"
+      exit 1
+else
+      echo "Ansible install success"
+dnf remove mysql -y
+dnf remove ansible -y
