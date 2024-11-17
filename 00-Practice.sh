c362d0cee0b12f59557be2a75c81bbe56704f7e7
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-11-17 11:52:25 +0530

    practice

--- 00-Practice.sh
+++ 00-Practice.sh
@@ -1,26 +1,26 @@
 #! /bin/bash/
 userid=$(id -u)
-if [ $userid -ne 0 ]
+if [ userid - ne 0 ]
 then
-      echo "please take Sudo Access"
-      exit 1
-else 
-      echo "you are a super user"
+       echo "Please start with Sudo Access"
+       exit 1
+else   
+       echo "Your a Super user"
 fi
 validate () {
-     if [ $1 -ne 0 ]
-     then
-           echo "$2....is failure"
-           exit 1
-     else 
-           echo "$2.....is Success"
-     fi
+      if [ $1 -ne 0 ]
+      then
+           echo "$2 is ......Failure"
+      else
+           echo "$2 is ......Success"
+      fi
 }
 dnf install mysql -y
-validate $? "installing my sql"
+validate $? "Installing mysql"
 dnf install ansible -y
-validate $? "insatlling ansible"
-dnf remove mysql -y 
+validate $? "installing ansible"
+dnf remove mysql -y
 validate $? "Removing mysql"
 dnf remove ansible -y
-validate $? "Removing ansible"
+validate $? "Removing Ansible"
+
